e1d34749a3ebad8d5af3d70b9442769224d2aa2e
Bhavik 9696 <raibhavik7619325139@gmail.com>
2025-09-05 13:24:40 +0530

    connect mongo-db

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 const express=require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app=express();
+
+app.use(express.json());
+
 
 app.get("/",(req,res)=>{
     res.json({msg:"hii"})
 })
 
 
+mongoose.connect(process.env.DB_URI).then(()=>{
+  console.log("✅ MongoDB connected");
+  
 app.listen(process.env.PORT || 5000,()=>{
   console.log(`server running on port http://localhost:${process.env.PORT}`);
+});
+
+}).catch((err)=>{
+   console.error("❌ MongoDB connection error:", err);
 })
