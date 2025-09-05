7d2a55f5832feb992e398e80c5bb6951e82e4d80
Bhavik 9696 <raibhavik7619325139@gmail.com>
2025-09-05 17:35:58 +0530

    change link to navlink in routing in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import { NavLink,Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
    const navigate = useNavigate();
@@ -16,15 +16,15 @@ const Navbar = () => {
   return (
       <header className="bg-white shadow">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
-        <Link to="/" className="text-xl font-bold">Q-Swap</Link>
+        <Link to="/" className=" font-bold">Q-Swap</Link>
         <nav className="flex items-center gap-4">
-          <Link to="/dashboard" className="text-sm">Dashboard</Link>
-          <Link to="/book" className="text-sm">Book Queue</Link>
-          <Link to="/helper" className="text-sm">Helper</Link>
+          <NavLink to="/dashboard" className={({isActive})=>isActive?" font-bold text-orange-400":" text-black"}>Dashboard</NavLink>
+          <NavLink to="/book" className={({isActive})=>isActive?" font-bold text-orange-400":" text-black"}>Book Queue</NavLink>
+          <NavLink to="/helper" className={({isActive})=>isActive?" font-bold text-orange-400":" text-black"}>Helper</NavLink>
           {!token ? (
             <>
-              <Link to="/login" className="text-sm">Login</Link>
-              <Link to="/signup" className="text-sm">Signup</Link>
+              <NavLink to="/login" className={({isActive})=>isActive?" font-bold text-orange-400":" text-black"}>Login</NavLink>
+              <NavLink to="/signup" className={({isActive})=>isActive?" font-bold text-orange-400":" text-black"}>Signup</NavLink>
             </>
           ) : (
             <>
