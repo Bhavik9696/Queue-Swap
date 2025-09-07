7a6d143e0437e76971e003ed4b9604cd2f2a5ae5
Bhavik 9696 <raibhavik7619325139@gmail.com>
2025-09-07 15:40:33 +0530

    connect all routes to server

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const { Server } = require('socket.io');
 const authRoutes = require('./routes/auth');
 const bookingRoutes = require('./routes/bookings');
 const paymentRoutes = require('./routes/payments');
-const helperRoutes = require('./routes/helpers');
 
 
 const app=express();
@@ -23,6 +22,42 @@ app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.get("/",(req,res)=>{
     res.json({msg:"Queue-Swap"})
 })
+
+app.use('/api/auth', authRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/payments', paymentRoutes);
+
+
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: process.env.FRONTEND_URL, methods: ['GET','POST'] }
+});
+
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth?.token;
+
+  socket.user = { id: token };
+  next();
+});
+
+io.on('connection', (socket) => {
+  console.log('socket connected', socket.id);
+  if (socket.user?.id) socket.join(`user:${socket.user.id}`);
+
+  socket.on('joinBookingRoom', (bookingId) => {
+    socket.join(`booking:${bookingId}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('socket disconnected', socket.id);
+  });
+});
+
+
+app.set('io', io);
 
 
 mongoose.connect(process.env.DB_URI).then(()=>{
